ce9474de16eb522924a23ba98c4c0ed515f33fab
scola <37su@163.com>
2020-07-19 23:45:17 +0000

    Move user model to models folder

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@
 });*/
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
